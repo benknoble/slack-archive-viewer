90e45c2c3160fe3d54f5a636b1775487cf629ced
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-08-09 14:35:24 -0400

    ->pollen: include metadata

--- private/pollen.rkt
+++ private/pollen.rkt
@@ -7,10 +7,14 @@
          racket/future
          racket/list
          racket/file
+         racket/path
          racket/function
          "json.rkt"
          "files.rkt"
-         "channels.rkt"
+         (only-in "channels.rkt" all-channel-files)
+         (rename-in "channels.rkt" [channels channel-names])
+         "meta.rkt"
+         (only-in "../merge-meta.rkt" users channels)
          "steps.rkt")
 
 (define (channel-json->pollen-text channel)
@@ -25,6 +29,9 @@
 (define-steps (->pollen data-dir)
   step "Make pollen directory tree"
   (mkdir! "pollen")
+  (for-each mkdir!
+            (map (compose1 (curry build-path "pollen") file-name-from-path)
+                 (channel-names data-dir)))
 
   step "Convert channel files to pollen"
   (define channel-file->pollen (compose1 channel-json->pollen-text file->json))
@@ -40,6 +47,22 @@
               [output-file output-files])
     (define pollen-text (channel-file->pollen channel-file))
     (display-lines-to-file pollen-text output-file))
-  output-files)
+
+  step "Convert metadata to jsond"
+  (define meta-files (list ((meta-info-make-path channels) data-dir)
+                           ((meta-info-make-path users) data-dir)))
+  (define output-meta-files
+    (map (λ (p)
+           (define with-racket-ext (path-replace-extension p ".rkt"))
+           (define bare-name (file-name-from-path with-racket-ext))
+           (define in-pollen (list "pollen" bare-name))
+           (path->complete-path (apply build-path in-pollen)))
+         meta-files))
+  (for/async ([meta-file meta-files]
+              [output-meta-file output-meta-files])
+    (define jsond-text (cons "#lang jsond" (cons "#:name meta" (file->lines meta-file))))
+    (display-lines-to-file jsond-text output-meta-file))
+
+  (values output-files output-meta-files))
 
 ;; vim: lw+=define-steps
