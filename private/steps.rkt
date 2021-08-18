757d60c954a4b1af30e3ccd4efaa809299977ed1
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-08-18 16:06:40 -0400

    steps: indent subordinate uses of steps

--- private/steps.rkt
+++ private/steps.rkt
@@ -5,19 +5,23 @@
 (require syntax/parse/define
          (for-syntax syntax/parse/lib/function-header))
 
+(define step-level (make-parameter -1))
+(define (indent) (build-string (* (step-level) 3) (λ _ #\space)))
+
 (begin-for-syntax
   (define-splicing-syntax-class step
     #:datum-literals (step)
     (pattern {~seq step message:string {~and form:expr {~not step}} ...})))
 
-(define-syntax-parse-rule (define-steps header:function-header step:step ...)
+(define-syntax-parse-rule (define-steps header:function-header step:step ...+)
   (define header
-    (define step-number -1)
-    (begin
-      (set! step-number (add1 step-number))
-      (displayln (format "~a. ~a" step-number step.message))
-      step.form ...)
-    ...))
+    (parameterize ([step-level (add1 (step-level))])
+      (define step-number -1)
+      (begin
+        (set! step-number (add1 step-number))
+        (displayln (format "~a~a. ~a" (indent) step-number step.message))
+        step.form ...)
+      ...)))
 
 (module+ test
   (require rackunit)
@@ -29,5 +33,24 @@
     step "Make a list"
     (list a b bar))
 
+  (define-steps (baz quux)
+    step "My variables"
+    (define c 3)
+    step "Make a list with sublists"
+    (list (foo c) quux))
+
+
   (check-equal? (with-output-to-string (thunk (check-equal? (foo 3) '(1 2 3))))
-                "0. Setup\n1. Make a list\n"))
+                "0. Setup\n1. Make a list\n")
+
+  (check-equal? (with-output-to-string (thunk (check-equal? (baz 3) '((1 2 3) 3))))
+                #<<EOS
+0. My variables
+1. Make a list with sublists
+   0. Setup
+   1. Make a list
+
+EOS
+                ))
+
+;; vim: lw+=define-steps
